3d4436ce7f98d3c58f404030b59a6ba98388c79a
Ramesh <rameshkjes@gmail.com>
2018-10-17 13:31:05 +0200

    query for search node with name added

--- rasa/queryGeneration.py
+++ rasa/queryGeneration.py
@@ -49,51 +49,47 @@ class GenerateQuery:
 
         return self.prediction
     
-    def convertTextToQuery(self, bundle_slot=None):
+    def getSimpleQuery(self, extracted_intent, extracted_value, query, query_result, params):
 
 
-        query = None
-        result = None
-        query_result = None
-        params = None
+        self.pypherObject.Match.node('u', labels=extracted_intent).WHERE.u.property('name') == extracted_value
+        query = str(self.pypherObject.RETURN.u)
+        params = self.pypherObject.bound_params
 
-        if self.extracted_intents == 'showProjectInformation':
-            
-            
-            # p.CREATE.node('user', 'User', Name='Jim')
-            # p.Match.node('a', labels='bundles').relationship('r').node('b', 'PackagesImports').RETURN('a', 'b', 'r')
-            
-            self.pypherObject.Match.node('u', labels=self.extracted_entities).WHERE.u.property('name') == self.extracted_values
-            query = str(self.pypherObject.RETURN.u)
-            params = self.pypherObject.bound_params
-            print ("\n ========== generated queries ===========")
-            print (query)
-            print (params)
-            
-            with self.driver.session() as  session:
-                result = session.run(str(self.pypherObject), **dict(self.pypherObject.bound_params))
+
+        with self.driver.session() as  session:
+                result = session.run(str(self.pypherObject), **dict(params))
 
                 # session.close()
                 # result = session.run(str(p))
                 print ("\n ========= result from neo4j ============ \n")
                 query_result = result.data()
 
+        return [query, params, query_result]
+
+
+    def convertTextToQuery(self, bundle_slot=None):
+
+        query = None 
+        query_result = None
+        params = None
+
+        if self.extracted_intents == 'showProjectInformation':
+            
+            [query, params, query_result] = self.getSimpleQuery(self.extracted_intents, self.extracted_values, query, query_result, params)
+            
+
         # this does not include show all bundles condition
         elif self.extracted_intents == 'showDetailedBundleProjectInfo':
 
-            self.pypherObject.Match.node('u', labels=self.extracted_entities).WHERE.u.property('name') == self.extracted_values
-            query = str(self.pypherObject.RETURN.u)
-            params = self.pypherObject.bound_params
-            print ("\n ========== generated queries ===========")
-            print (query)
-            print (params)
-            
-            with self.driver.session() as  session:
-                result = session.run(str(self.pypherObject), **dict(self.pypherObject.bound_params))
+            [query, params, query_result] = self.getSimpleQuery(self.extracted_intents, self.extracted_values, query, query_result, params)
 
-                print ("\n ========= result from neo4j ============ \n")
-                query_result = result.data()
 
+        # show specific node information
+        elif self.extracted_intents == 'showNodeInformation':
+
+            [query, params, query_result] = self.getSimpleQuery(None, self.extracted_values, query, query_result, params)
+  
         # exports inside bundles
         elif self.extracted_intents == 'showExportsInBundle':
 
@@ -129,14 +125,3 @@ if __name__ == '__main__':
     
     generateQuery = GenerateQuery(nlu_interpreter)
     generateQuery.predictIntentionAndEntity()
-
-# sentence = "look for description of de.rcenvironment.components.excel.common.ExcelService service"
-# sentence = "Bye"
-# # sentence = "list packages.24 packages available in Car bundle"
-# print ("Original sentence: \n", sentence)
-# print ("--------------------------------------------")
-# gQuery = GenerateQuery(train, sentence)
-# gQuery.predictIntentionAndEntity()
-# gQuery.convertTextToQuery()
-
-## To do: Make code more shorter by creating function
