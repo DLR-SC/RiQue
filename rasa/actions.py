38bceb72a1a30dee1be7ca40f0472dace741bc89
Ramesh <rameshkjes@gmail.com>
2018-10-30 16:11:22 +0100

    restaurant actions removed and show detailed bundle info action added

--- rasa/actions.py
+++ rasa/actions.py
@@ -6,95 +6,6 @@ from pypher.builder import Param
 from queryGeneration import GenerateQuery
 from rasa_core.events import AllSlotsReset, Restarted
 
-class ActionCheckRestaurants(Action):
-
-    def name(self):
-      # type: () -> Text
-      return "action_check_restaurants"
-
-    def run(self, dispatcher, tracker, domain):
-      # type: (Dispatcher, DialogueStateTracker, Domain) -> List[Event]
-
-      info = tracker.get_slot('GPE')
-      dispatcher.utter_message(info)
-      # info2 = tracker.get_slot('restaurant')
-      # dispatcher.utter_message(info2)
-
-      result = None
-      
-      # self.recent_message = (tracker.latest_message)['text']
-      # dispatcher.utter_message(self.recent_message)
-      
-      dispatcher.utter_message(" ==== current state of tracker ======")
-      
-      # dispatcher.utter_message(tracker.current_state())
-
-
-      # message = tracker.latest.text 
-      # operate on(message)
-
-      if tracker.get_slot('GPE'):
-
-
-          with open('restaurants.txt') as file:
-            data = file.read()
-            dispatcher.utter_message("list of restaurants in tracker.get_slot('city')")
-            dispatcher.utter_message(data)
-
-          dispatcher.utter_message("action_check_restaurants being called ..")
-
-      else:
-          dispatcher.utter_message("no GPE slot filled inside action check restaurant")
-          
-      # cuisine = tracker.get_slot('cuisine')
-      # q = "select * from restaurants where cuisine='{0}' limit 1".format(cuisine)
-      # result = db.query(q)
-
-      # return [SlotSet("matches", result if result is not None else [])]
-
-      return []
-      
-
-class showDetailedRestaurantInfo(Action):
-
-
-    def name(self):
-      # type: () -> Text
-      return "action_show_detailed_restaurant_info"
-
-    def run(self, dispatcher, tracker, domain):
-      # type: (Dispatcher, DialogueStateTracker, Domain) -> List[Event]
-
-      info = tracker.get_slot('GPE')
-      dispatcher.utter_message('City name: ')
-      dispatcher.utter_message(info)
-      info2 = tracker.get_slot('restaurant')
-      dispatcher.utter_message('restaurant name: ')
-      dispatcher.utter_message(info2)
-
-
-      result = None
-      
-      # self.recent_message = (tracker.latest_message)['text']
-      # dispatcher.utter_message(self.recent_message)
-            
-      # dispatcher.utter_message(tracker.current_state())
-
-      # message = tracker.latest.text 
-      # operate on(message)
-
-      if tracker.get_slot('restaurant'):
-
-          result = "restaurant slot found"
-          dispatcher.utter_message(" It is located near HBF. ")
-
-      else:
-          dispatcher.utter_message("no restaurant in particular city slot being called")
-
-
-
-      return []
-
 class DisplayGeneralQuery(Action):
 
 
@@ -111,29 +22,14 @@ class DisplayGeneralQuery(Action):
       recent_message = (tracker.latest_message)['text']
 
       dispatcher.utter_message(" ==== current state of tracker ======")
-      # dispatcher.utter_message(tracker.current_state)
 
-      result = None
-      failure = None
       dispatcher.utter_message("project slot value")
       dispatcher.utter_message(tracker.get_slot('project'))
+
       if tracker.get_slot('project'):
 
           displayQueryOutput(recent_message, dispatcher)
-          # gQuery = GenerateQuery(recent_message)
-          # parse_msg = gQuery.predictIntentionAndEntity()
-          
-          # [query, params, result] = gQuery.convertTextToQuery()
-          # dispatcher.utter_message("===== Query =====")
-          # dispatcher.utter_message(query)
-          # dispatcher.utter_message("=== query params =====")
-          # dispatcher.utter_message(str(params))
-          # dispatcher.utter_message("===== result =====")
-          # dispatcher.utter_message(str(result))
 
-          # print ("parse msg")
-
-          # result = "project info show bitte"
           dispatcher.utter_message("project info being uttered ..")
 
       else:
@@ -143,39 +39,29 @@ class DisplayGeneralQuery(Action):
       return []
 
 
-class DisplayBundleQuery(Action):
+class DisplayBundleDetailedQuery(Action):
 
 
     def name(self):
 
-      return "action_show_detailed_bundle_project_info"
+      return "action_show_show_detail_info_bundles"
 
     def run(self, dispatcher, tracker, domain):
       # type: (Dispatcher, DialogueStateTracker, Domain) -> List[Event]
 
     # tracker.get_slot('city')[bool(tracker.get_slot('city'))] 
-      dispatcher.utter_message("action give **detailed** project info being called ")
+      dispatcher.utter_message("action show detail info bundle")
 
       recent_message = (tracker.latest_message)['text']
 
       # dispatcher.utter_message(tracker.current_state)
 
-      result = None
-      failure = None
+      if tracker.get_slot('BundlesName'):
 
-      if tracker.get_slot('bundles'):
-
-          displayQueryOutput(recent_message, dispatcher)
-          # gQuery = GenerateQuery(recent_message)
-          # parse_msg = gQuery.predictIntentionAndEntity()
+          dispatcher.utter_message("Slot value ")
+          dispatcher.utter_message(tracker.get_slot('BundlesName'))
           
-          # [query, params, result] = gQuery.convertTextToQuery()
-          # dispatcher.utter_message("===== Query =====")
-          # dispatcher.utter_message(query)
-          # dispatcher.utter_message("=== query params =====")
-          # dispatcher.utter_message(str(params))
-          # dispatcher.utter_message("===== result =====")
-          # dispatcher.utter_message(str(result))
+          displayQueryOutput(recent_message, dispatcher)
 
           dispatcher.utter_message("bundles slot found and action bundle executed")
 
@@ -208,17 +94,6 @@ class DisplayServiceQuery(Action):
       failure = None
 
       if tracker.get_slot('service'):
-
-          # gQuery = GenerateQuery(recent_message)
-          # parse_msg = gQuery.predictIntentionAndEntity()
-          
-          # [query, params, result] = gQuery.convertTextToQuery()
-          # dispatcher.utter_message("===== Query =====")
-          # dispatcher.utter_message(query)
-          # dispatcher.utter_message("=== query params =====")
-          # dispatcher.utter_message(str(params))
-          # dispatcher.utter_message("===== result =====")
-          # dispatcher.utter_message(str(result))
 
           dispatcher.utter_message("service slot found and action service executed")
       else:
@@ -255,18 +130,6 @@ class DisplayExportQuery(Action):
       if tracker.get_slot('PackagesExports'):
 
           displayQueryOutput(recent_message, dispatcher)
-          # gQuery = GenerateQuery(recent_message)
-          # parse_msg = gQuery.predictIntentionAndEntity()
-          
-          # [query, params, result] = gQuery.convertTextToQuery(bundle_slot)
-          # dispatcher.utter_message("===== Query =====")
-          # dispatcher.utter_message(query)
-          # dispatcher.utter_message("=== query params =====")
-          # dispatcher.utter_message(str(params))
-          # dispatcher.utter_message("===== result =====")
-          # dispatcher.utter_message(str(result))
-
-
           dispatcher.utter_message("bundles slot found and action bundle executed")
 
       else:
@@ -296,18 +159,6 @@ class showNodeInformation(Action):
       if tracker.get_slot('node'):
 
           displayQueryOutput(recent_message, dispatcher)
-          # gQuery = GenerateQuery(recent_message)
-          # parse_msg = gQuery.predictIntentionAndEntity()
-          
-          # [query, params, result] = gQuery.convertTextToQuery()
-          # dispatcher.utter_message("===== Query =====")
-          # dispatcher.utter_message(query)
-          # dispatcher.utter_message("=== query params =====")
-          # dispatcher.utter_message(str(params))
-          # dispatcher.utter_message("===== result =====")
-          # dispatcher.utter_message(str(result))
-
-
           dispatcher.utter_message("node slot got filled and action show node information executed")
 
       else:
@@ -444,4 +295,3 @@ def displayQueryOutput(recent_message, dispatcher, bundle_slot=None):
   dispatcher.utter_message(str(params))
   dispatcher.utter_message("===== result =====")
   dispatcher.utter_message(str(result))
-
