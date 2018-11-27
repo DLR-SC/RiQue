d55a16c463f6075f5da2312c5dfc830e9d2dc896
Ramesh <ramesh.kumar@dlr.de>
2018-11-27 14:35:49 +0100

    standarized intent name function added

--- SentenceToQuery/utils.py
+++ SentenceToQuery/utils.py
@@ -6,7 +6,7 @@ class Utility:
 
 		gQuery = GenerateQuery(recent_message)
 		parse_msg = gQuery.predictIntentionAndEntity()
-		[query, params, result] = gQuery.convertTextToQuery(bundle_slot)
+		[query, params, result, extracted_intent] = gQuery.convertTextToQuery(bundle_slot)
 		
 		# restructure query
 		for key, value in params.items():
@@ -17,7 +17,26 @@ class Utility:
 
 		# dispatcher.utter_message("===== Query =====")
 		dispatcher.utter_message(query)
+		intents = self.standarizeIntentNames()
+		dispatcher.utter_message(intents[str(extracted_intent)])
 		# dispatcher.utter_message("=== query params =====")
 		# dispatcher.utter_message(str(params))
 		# dispatcher.utter_message("===== result =====")
 		# dispatcher.utter_message(str(result))
+
+	def standarizeIntentNames(self):
+
+		intents = {}
+		intents['greet'] = 'Greet'
+		intents['goodbye'] = 'Good Bye'
+		intents['showProjectInformation'] = 'Show project information'
+		intents['showNodeInformation'] = 'Show node information'
+		intents['showAllNodes'] = 'Show all nodes'
+		intents['my_name_is'] = 'Greet with name'
+		intents['countAllNodes'] = 'Count all nodes'
+		intents['showDetailInfoBundles'] = 'Show information about specific bundle'
+		intents['showLargestCompilationUnit'] = 'Show largest compilation unit'
+
+		return intents
+
+
